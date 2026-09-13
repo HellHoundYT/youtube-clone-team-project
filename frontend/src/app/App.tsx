ceebf675cb2345ab7b13df50039700fc0193b306
HellHoundYT <houndshell57@gmail.com>
2026-09-13 23:10:49 +0300

    feat(auth): protect upload and profile routes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -7,6 +7,7 @@ import {
   useEffect,
 } from 'react'
 import AppLayout from '../presentation/components/layout/AppLayout'
+import RequireAuth from '../presentation/components/auth/RequireAuth'
 import AuthPage from '../presentation/pages/AuthPage'
 import ChannelPage from '../presentation/pages/ChannelPage'
 import CategoryPage from '../presentation/pages/CategoryPage'
@@ -146,17 +147,19 @@ function App() {
           }
         />
 
-        <Route
-          path="upload"
-          element={
-            <UploadPage uploadService={uploadService} />
-          }
-        />
+        <Route element={<RequireAuth />}>
+          <Route
+            path="upload"
+            element={
+              <UploadPage uploadService={uploadService} />
+            }
+          />
 
-        <Route
-          path="profile"
-          element={<ProfilePage />}
-        />
+          <Route
+            path="profile"
+            element={<ProfilePage />}
+          />
+        </Route>
 
         <Route
           path="watch-party"
